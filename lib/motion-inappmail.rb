a89e910f517f8fdf33045873081f3bfed3dd1e38
Gaël Gillard <gillardgael@gmail.com>
2013-04-07 17:24:49 +0200

    convert indentation

--- lib/motion-inappmail.rb
+++ lib/motion-inappmail.rb
@@ -1,7 +1,7 @@
 unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
- 
+
 Motion::Project::App.setup do |app|
   app.frameworks << 'MessageUI'
   Dir.glob(File.join(File.dirname(__FILE__), 'motion-inappmail/*.rb')).each do |file|
--- lib/motion-inappmail/inappmail.rb
+++ lib/motion-inappmail/inappmail.rb
@@ -1,49 +1,49 @@
 module InAppMail
-	module_function
+  module_function
 
-	def create(delegate,options,&callback)
-		@callback = callback
+  def create(delegate,options,&callback)
+    @callback = callback
     @delegate = delegate
 
-		@mailController = MFMailComposeViewController.alloc.init
-		@mailController.mailComposeDelegate = self
+    @mailController = MFMailComposeViewController.alloc.init
+    @mailController.mailComposeDelegate = self
 
-		if((options)&&(options.class == Hash))
-			if((options[:to])&&(options[:to].class == Array))
-				@mailController.setToRecipients(options[:to])
-			end
+    if((options)&&(options.class == Hash))
+      if((options[:to])&&(options[:to].class == Array))
+        @mailController.setToRecipients(options[:to])
+      end
 
-			if((options[:cc])&&(options[:cc].class == Array))
-				@mailController.setCcRecipients(options[:cc])
-			end
+      if((options[:cc])&&(options[:cc].class == Array))
+        @mailController.setCcRecipients(options[:cc])
+      end
 
-			if((options[:bcc])&&(options[:bcc].class == Array))
-				@mailController.setBccRecipients(options[:bcc])
-			end
+      if((options[:bcc])&&(options[:bcc].class == Array))
+        @mailController.setBccRecipients(options[:bcc])
+      end
 
-			if((options[:subject])&&(options[:subject].class == String))
-				@mailController.setSubject(options[:subject])
-			else
+      if((options[:subject])&&(options[:subject].class == String))
+        @mailController.setSubject(options[:subject])
+      else
         @mailController.setSubject("Contact")
       end
 
-			if((options[:message])&&(options[:message].class == Hash))
-				if(options[:message][:html])
-					@isHtml = true
-				else
-					@isHtml = false
-				end
-				if((options[:message][:body])&&(options[:message][:body].class == String))
-					@mailController.setMessageBody(options[:message][:body], isHTML: @isHtml)
-				end
-			end
-		end
+      if((options[:message])&&(options[:message].class == Hash))
+        if(options[:message][:html])
+          @isHtml = true
+        else
+          @isHtml = false
+        end
+        if((options[:message][:body])&&(options[:message][:body].class == String))
+          @mailController.setMessageBody(options[:message][:body], isHTML: @isHtml)
+        end
+      end
+    end
 
-		@delegate.presentModalViewController(@mailController, animated:true)
-	end
+    @delegate.presentModalViewController(@mailController, animated:true)
+  end
 
-	def mailComposeController(controller, didFinishWithResult: result, error: error)
-		@delegate.dismissModalViewControllerAnimated(true)
-		@callback.call result: result, error: error
-	end
+  def mailComposeController(controller, didFinishWithResult: result, error: error)
+    @delegate.dismissModalViewControllerAnimated(true)
+    @callback.call result: result, error: error
+  end
 end
